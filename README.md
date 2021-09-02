3d3d9db5d679795164e0389c0c01287ce79cf4f3
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2021-09-02 16:38:48 +1200

    Initial Data analysis

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# prison-transfers
+# Inter Prison Transfers in Aotearoa/New Zealand
+
+## FOIA - Inter-Prison Transfers Across Time and Place
+
+https://fyi.org.nz/request/16105-inter-prison-transfers-across-time-and-place#outgoing-27826
+
+```
+Tēnā koutou,
+
+I'm interested in tracking non-identifying data on prisoner transfers within the prison system in Aotearoa.
+
+If possible I'd like data for prisoners sentenced and on remand for the period 2011-2021 and specifically:
+
+- the date of transfer
+- the name of the prison that the prisoner was transferred from
+- the name of the prison that the prisoner was transferred to
+
+It would be really helpful if you could also include:
+
+- the reason for their transfer
+- where an individual has been transferred multiple times would it be possible to assign that person a non-identifying code so it's possible to track the prison facilities they have been incarcerated in?
+
+Please provide this information in an accessible, searchable format. I would prefer to receive the requested figures in a machine-readable spreadsheet format, such as CSV or XLSX.
+
+If any part of my request is unclear, please don't hesitate to contact me.
+
+If any of the information that I have requested is unavailable or would be difficult to retrieve, but similar information might be readily available, I would be happy to discuss altering or refining my request.
+
+If any part of my request is to be refused, under section 19(a)(ii) of the Official Information Act please provide the grounds in support of the reason for refusal.
+Also, if the time limit for this request is extended under section 15A(1)(b) of the Official Information Act, please explain what consultations are necessary to make a decision on my request, why they are necessary, and why they could not be reasonably completed within the original time limit.
+
+Ngā mihi,
+
+Fraser Crichton
+```
+
+
+
+https://www.corrections.govt.nz/resources/policy_and_legislation/Prison-Operations-Manual/Movement/M.04-External-movement-transportation-of-prisoners/M.04.03-Inter-prison-transfers
+
+
+    Population Pressure - Adviser Prison Population only
+        For muster management purpose
+        To separate different categories of prisoners
+    Judicial - RO staff, PCO of the unit or Board Liaison Officers
+        For Judicial purposes
+        The transfer is pursuant to section 143 of the Sentencing Act 2002
+    Medical - Health staff
+        to provide Health care
+    Placement Management - Prison Director, Residential Manager or  PCO
+        to ensure the safety of that prisoner or any other reason
+        to reduce the risk of suicide or self-harm
+        transfer has been directed by the Chief Executive or an Inspector
+        to restore or maintain security and order of the prison
+    Personal Request - PCO
+        prisoner request transfer
+    Accepted for Programme - PCO or delegate from the destination unit
+        for reintegration / rehabilitation purposes
+        to address offender plan activities
+    Release - PCO or RO staff
+        to be close to home region
+    Placement Review - PCO
+        to assist in likelihood of reducing re-offending
+        due to change in security classification
+        to allow for repairs or alterations at the prison
+
+## Python Deck GL
+
+## 
---
+++ data_analysis.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import pydeck as pdk
+
+prison_locations = 'data/prisons.json'
+transfers = 'data/transfers.csv'
+
+
+locations_df = pd.read_json(prison_locations)
+transfers_df = pd.read_csv(transfers)
+
+dict = pd.DataFrame([{
+    'From': 'Rimutaka',
+    'To': 'SHCF',
+    'Reason': 'Judicial'
+}])
+
+df = pd.merge(transfers_df, locations_df, left_on='From',
+              right_on='Abbreviated',
+              how='left')
+
+df['Transfer Date'] = pd.to_datetime(df['Transfer Date'])
+
+print(df['Transfer Date'])
+
+df['Transfer_From_Coordinates'] = df[['Longitude', 'Latitude']].values.tolist()
+df['From_Coordinates'] = df[['Latitude', 'Longitude', ]].values.tolist()
+
+df = df.drop(columns=['Name', 'Prison', 'Abbreviated', 'Official Name', 'Gender', 'Opened', 'Closed', 'Capacity',
+                      'Classification', 'Latitude', 'Longitude'])
+
+df = pd.merge(df, locations_df, left_on='To',
+              right_on='Abbreviated',
+              how='left')
+
+df['Transfer_To_Coordinates'] = df[['Longitude', 'Latitude']].values.tolist()
+
+df = df.drop(columns=['Name', 'Prison', 'Abbreviated', 'Official Name', 'Gender', 'Opened', 'Closed', 'Capacity',
+                      'Classification', 'Latitude', 'Longitude'])
+print(df['Reason'].value_counts())
+print(df[['From']].value_counts())
+print(df[['To']].value_counts())
+
+print(df.describe())
+
+BLUE_RGB = [0, 0, 255, 40]
+RED_RGB = [240, 100, 0, 40]
+
+
+
+def get_layers(data_frame):
+    arc_layer = pdk.Layer(
+        "ArcLayer",
+        data=data_frame,
+        get_width="2",
+        get_source_position="Transfer_From_Coordinates",
+        get_target_position="Transfer_To_Coordinates",
+        get_tilt=90,
+
+        get_source_color=RED_RGB,
+        get_target_color=BLUE_RGB,
+        pickable=True,
+        auto_highlight=True,
+    )
+
+    layer = pdk.Layer(
+        "GridLayer",
+        data=data_frame,
+        pickable=True,
+        extruded=True,
+        cell_size=20000,
+        elevation_scale=400,
+        get_position='Transfer_To_Coordinates',
+        colorRange=[
+            BLUE_RGB,
+        ]
+    )
+    from_layer = pdk.Layer(
+        "GridLayer",
+        data=data_frame,
+        pickable=True,
+        extruded=True,
+        cell_size=20000,
+        elevation_scale=400,
+        get_position='Transfer_From_Coordinates',
+        colorRange=[
+            RED_RGB,
+        ]
+    )
+    return [arc_layer, from_layer]
+
+
+# GET_COLOR_JS = [
+#     "255 * (1 - (Transfer_From_Coordinates[2] / 10000) * 2)",
+#     "128 * (Transfer_From_Coordinates[2] / 10000)",
+#     "255 * (Transfer_From_Coordinates[2] / 10000)",
+#     "255 * (1 - (Transfer_From_Coordinates[2] / 10000))",
+# ]
+# line_layer = pdk.Layer(
+#     "LineLayer",
+#     data=df,
+#     get_source_position="Transfer_From_Coordinates",
+#     get_target_position="Transfer_To_Coordinates",
+#     get_color=GET_COLOR_JS,
+#     get_width=2,
+#     highlight_color=[255, 255, 0],
+#     picking_radius=10,
+#     auto_highlight=True,
+#     pickable=True,
+# )
+#
+#  mapboxApiAccessToken={MAP_BOX_ACCESS_TOKEN}
+#           mapStyle={MAP_BOX_STYLE_ID}
+def view(html_file_name, layers):
+    view_state = pdk.ViewState(longitude=174.1886324, latitude=-40.6509396, bearing=0, pitch=45, zoom=5)
+    TOOLTIP_TEXT = {
+        "html": "From: {From} To: {To} Reason: {Reason} Transfer Date: {Transfer Date} Status At Transfer: {Status At Transfer} <br /> From in red; To in blue"}
+    r = pdk.Deck(layers, map_style='light', initial_view_state=view_state, tooltip=TOOLTIP_TEXT)
+    r.to_html(html_file_name)
+
+
+view("arc_layer.html", get_layers(df))
+# 23 March 2020	Negative increase 3
+# 26 March 2020	Negative increase 4
+# 28 April 2020	Positive decrease 3
+# 14 May 2020	Positive decrease 2
+# 9 June 2020	Positive decrease 1
+mask = (df['Transfer Date'] > '2020-03-26') & (df['Transfer Date'] <= '2020-04-28')
+df = df.loc[mask]
+
+# From,To,Reason,Transfer Date,Status At Transfer
+print(df['Reason'].value_counts())
+print(df[['From']].value_counts())
+print(df[['To']].value_counts())
+
+# print(df[['From', 'To']].value_counts())
+
+# print(df['To'].value_counts())
+view("arc_layer_covid.html", get_layers(df))
+
+# trace through In Transit/Multi-Site Move     832
+# where os the place reviews end up
